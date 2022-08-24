8f857e8e2e9e52d5e2a204e40adc23b99d1263fc
Alex <breuslife@gmail.com>
2022-08-24 20:56:26 +0300

    Add Pokemon model & add in admin.py

--- apps/pokemons/admin.py
+++ apps/pokemons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from pokemons.models import Pokemon
+
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    """For future customizations"""
+
--- apps/pokemons/models.py
+++ apps/pokemons/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(_('Name'), max_length=255)
+    description = models.TextField(_('Description'), blank=True, null=True)
+
+    height = models.IntegerField(_('Height'))
+    weight = models.IntegerField(_('Weight'))
+
+    order = models.IntegerField(_('Order'))
+
+    class Meta:
+        db_table = 'pokemons'
+        verbose_name = _('Pokemon')
+        verbose_name_plural = _('Pokemons')
+
+    def __str__(self):
+        return self.name
